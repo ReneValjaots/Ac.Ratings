b2d5a9e60227267ab9e887790ec1ed8e7b7644b3
ReneValjaots <rene.valjaots@gmail.com>
2025-03-21 01:34:43 +0200

    v0.9.1.4 - Primary sliders now change color based on accentcolor

---
+++ Ac.Ratings/Theme/Converters/DarkenColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Ac.Ratings.Theme.Converters;
+
+public class DarkenColorConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        if (value is Color color && parameter is string param && double.TryParse(param, out double factor)) {
+            factor = Math.Clamp(factor, 0, 1);
+            return Color.FromArgb(
+                color.A,
+                (byte)(color.R * (1 - factor)),
+                (byte)(color.G * (1 - factor)),
+                (byte)(color.B * (1 - factor)));
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        throw new NotImplementedException();
+    }
+}
---
+++ Ac.Ratings/Theme/Converters/LightenColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Ac.Ratings.Theme.Converters;
+
+public class LightenColorConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        if (value is Color color && parameter is string param && double.TryParse(param, out double factor)) {
+            factor = Math.Clamp(factor, 0, 1);
+            return Color.FromArgb(
+                color.A,
+                (byte)Math.Min(255, color.R + (255 - color.R) * factor),
+                (byte)Math.Min(255, color.G + (255 - color.G) * factor),
+                (byte)Math.Min(255, color.B + (255 - color.B) * factor));
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        throw new NotImplementedException();
+    }
+}
